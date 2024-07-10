2fbaeb237935fb2b3122bb06eee4ae6b6af02728
Muhammad Rizky Anditama <rizkind@gmail.com>
2024-07-10 23:44:23 +0700

    init marketpalce orderbackend app

---
+++ marketplace_order_backend/src/main/java/com/marketplace/orderapp/marketplace_order_backend/MarketplaceOrderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.orderapp.marketplace_order_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceOrderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceOrderBackendApplication.class, args);
+	}
+
+}
---
+++ marketplace_order_backend/src/test/java/com/marketplace/orderapp/marketplace_order_backend/MarketplaceOrderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.orderapp.marketplace_order_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceOrderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
