7dfdf02e8cdb5d6af27103d28de606c939df60c7
Muhammad Rizky Anditama <rizkind@gmail.com>
2024-07-12 21:23:33 +0700

    init product app

---
+++ marketplace_product_backend/src/main/java/com/marketplace/productapp/marketplace_product_backend/MarketplaceProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.productapp.marketplace_product_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceProductBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceProductBackendApplication.class, args);
+	}
+
+}
---
+++ marketplace_product_backend/src/test/java/com/marketplace/productapp/marketplace_product_backend/MarketplaceProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.productapp.marketplace_product_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
