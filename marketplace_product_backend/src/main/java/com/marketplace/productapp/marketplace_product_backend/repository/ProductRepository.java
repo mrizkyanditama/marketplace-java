4e2b9a6234e61b9e10aa891703ccd4270495e956
Muhammad Rizky Anditama <rizkind@gmail.com>
2024-07-12 21:53:44 +0700

    implement repository

---
+++ marketplace_product_backend/src/main/java/com/marketplace/productapp/marketplace_product_backend/repository/ProductRepository.java
@@ -0,0 +1,39 @@
+package com.marketplace.productapp.marketplace_product_backend.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.marketplace.productapp.marketplace_product_backend.model.Product;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+
+@Repository
+public class ProductRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Product> findProductsByTagName(String tagName) {
+        String sql = "SELECT p.* FROM Product p " +
+                     "JOIN Product_Tag pt ON p.product_id = pt.product_id " +
+                     "JOIN Tag t ON pt.tag_id = t.tag_id " +
+                     "WHERE t.name = :tagName";
+
+        return entityManager.createNativeQuery(sql, Product.class)
+                          .setParameter("tagName", tagName)
+                          .getResultList();
+    }
+
+    public List<Product> findProductsByProductIds(List<String> productIds) {
+        String jpql = "SELECT p FROM Product p " +
+                      "WHERE p.productId IN :productIds";
+
+        TypedQuery<Product> query = entityManager.createQuery(jpql, Product.class);
+        query.setParameter("productIds", productIds);
+        return query.getResultList();
+    }
+
+}
